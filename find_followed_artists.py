3d983a6500ebfc032a38eb38b146a3a30b0ed93d
Calvin Mclean <calvinmclean@email.arizona.edu>
2017-01-14 19:34:26 -0700

    python files for finding user music prefs

---
+++ find_followed_artists.py
@@ -0,0 +1,25 @@
+import sys
+import spotipy
+import spotipy.util as util
+import json
+
+scope = 'user-follow-read'
+
+username = 'calvinm13'
+
+token = util.prompt_for_user_token(username, scope, '8e5cac0ed299444480f463d1a662829f', '078527b8e4394a1c8efd5c07bce12568', 'http://localhost/')
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_followed_artists()
+    # print json.dumps(results, indent=2, sort_keys=True)
+else:
+    print "Can't get token for", username
+
+artists = []
+
+# Create simple array of artists
+for item in results['artists']['items']:
+    artists.append(item['name'])
+
+print str(artists)
